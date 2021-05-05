a622edab0458290613c8b3408f996a1f706db34e
jwyatt1995@gmail.com <jwyatt1995@gmail.com>
2021-05-05 09:06:49 +0100

    Fix bad import

--- src/pages/venue.js
+++ src/pages/venue.js
@@ -1,6 +1,5 @@
 import React from "react"
 import Layout from "../components/Layout"
-import { url } from "../styles/venue.module.css"
 export default function Venue() {
   return (
     <Layout>
@@ -9,9 +8,7 @@ export default function Venue() {
       <p>Waxham Great Barn, Coast Road, Waxham, Norfolk, NR12 0DZ</p>
       <p>
         Directions to the venue can be found on{" "}
-        <a className={url} href="https://goo.gl/maps/MGSQoTf3ywewanKh9">
-          Google Maps
-        </a>
+        <a href="https://goo.gl/maps/MGSQoTf3ywewanKh9">Google Maps</a>
       </p>
     </Layout>
   )
